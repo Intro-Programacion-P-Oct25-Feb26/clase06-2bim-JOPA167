3b6eb4bfb722a810dce3b39130ea5ef4627e73d6
JOPA167 <joparajirj@gmail.com>
2026-01-21 12:35:18 -0500

    commit final

--- EjemplosExcepciones/src/paquete02/Ejercicioclase01.java
+++ EjemplosExcepciones/src/paquete02/Ejercicioclase01.java
@@ -13,15 +13,16 @@ import java.util.Scanner;
  */
 public class Ejercicioclase01 {
 
-    public static void main(String arg) {
+    public static void main(String[] args) {
 
-        int valor1;
-        int valor2;
-        int resultado;
+        int valor1 = obtenerValor();
+        int valor2 = obtenerValor();
+        int resultado = obtenerSuma(valor1, valor2);
+        reporteFinal(valor1, valor2,resultado);
 
     }
 
-    public static int obtenerValor() throws Exception {
+    public static int obtenerValor(){
         Scanner entrada = new Scanner(System.in);
         boolean bandera = true;
         int valor1 = 0;
@@ -39,12 +40,23 @@ public class Ejercicioclase01 {
                 System.out.printf("(InputMismatchException) Ocurrió una "
                         + "excepción %s\n", e);
             }catch (Exception e){
-                 System.out.printf("(InputMismatchException) Ocurrió una "
+                 System.out.printf("Ocurrió una "
                     + "excepción %s\n", e);
             }
 
         }
         return valor1;
+    }
+    
+    public static int obtenerSuma (int valor1,  int valor2 ){
+        int suma;
+        suma = valor1 + valor2;
+        return suma;
+    }
+    public static void reporteFinal(int a, int b, int suma){
+        String mensaje = "";
+        mensaje = String.format("La suma entre %d y %d es: %d", a,b,suma);
+        System.out.printf("%s\n",mensaje);
     }
 }
 // enteros - pares
